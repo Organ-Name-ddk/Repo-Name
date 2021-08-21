620b51813555ac3d8497c51c96e1210cb1b05dd3
王硕 <1485217094@qq.com>
2021-08-21 13:27:09 +0800

    分支提交

--- demo/src/main/java/com/fc/controller/UserController.java
+++ demo/src/main/java/com/fc/controller/UserController.java
@@ -5,15 +5,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author ddk
- * 2021/8/21
+ *  2021/8/21
  */
 @RestController
 public class UserController {
 
     @GetMapping("/test")
-    public String test() {
-        System.out.println("");
+    public String test(){
         System.out.println("www");
         return "wsest";
-    }
-}
+            }
+        }
