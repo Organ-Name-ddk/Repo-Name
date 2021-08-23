39a588e226a3b11e0a53fd7830e7ed9b06ed21b0
王硕 <1485217094@qq.com>
2021-08-23 10:07:32 +0800

    分支提交代码

--- demo/src/main/java/com/fc/controller/UserController.java
+++ demo/src/main/java/com/fc/controller/UserController.java
@@ -7,18 +7,18 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * @author ddk
- *  2021/8/21
+ * 2021/8/21
  */
 @RestController
 public class UserController {
 
     @GetMapping("/test")
-    public String test(){
+    public String test() {
         try {
             TimeUnit.SECONDS.sleep(1);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         return null;
-            }
-        }
+    }
+}
