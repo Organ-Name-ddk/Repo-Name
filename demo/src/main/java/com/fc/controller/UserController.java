fd69a8eca30a1d3d038bbcdef8d68c45428809ea
王硕 <1485217094@qq.com>
2021-08-23 10:07:19 +0800

    分支提交代码

--- demo/src/main/java/com/fc/controller/UserController.java
+++ demo/src/main/java/com/fc/controller/UserController.java
@@ -3,6 +3,8 @@ package com.fc.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author ddk
  *  2021/8/21
@@ -12,10 +14,11 @@ public class UserController {
 
     @GetMapping("/test")
     public String test(){
-        System.out.println("000");
-        System.out.println("www");
-        System.out.println("sss");
-
-        return "wsest";
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return null;
             }
         }
